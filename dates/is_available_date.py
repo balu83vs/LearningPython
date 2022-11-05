6c7391df618dc61e3bc9306c9c79227c146bc7e3
Vitaliy <balu83vs@gmail.com>
2022-11-05 22:09:56 +0300

    Final commit 05/11/2022

--- dates/is_available_date.py
+++ dates/is_available_date.py
@@ -1,34 +1,38 @@
-from datetime import date, datetime
+                                                                                              # программа для определения правильности бронирования
 
-def is_available_date(dates, some_date):
-    temp = []
-    temp2 = []
-    temp3 = []
-    temp4 = []
-    for el in dates:
-        temp.append(el.split('-'))
+from datetime import datetime
+
+def is_available_date(dates, some_date):                                                      # основная функция
+    stop_num = []
+    in_date = []
+
+    stop_date = [el.split('-') for el in dates]                                               # заполняем список значений занятых дат
     
-    for el in temp:
-        el = list(map(lambda x: str(int(datetime.strptime(x, '%d.%m.%Y').timestamp())), el))
-        temp2.append(el)  
+    for el in stop_date:
+        el = list(map(lambda x: str(int(datetime.strptime(x, '%d.%m.%Y').timestamp())), el))  # создание списка занятых дат в представлении timestamp
+        stop_num.append(el)     
 
-    temp3 = some_date.split('-')
+    in_date = some_date.split('-')                                                            # список желаемых дат бронирования
+    in_num = [str(int(datetime.strptime(el, '%d.%m.%Y').timestamp())) for el in in_date]      # список желаемых дат в представлении  timestamp
 
-    for el in temp3:
-        el = str(int(datetime.strptime(el, '%d.%m.%Y').timestamp()))
-        temp4.append(el)
-
-
-    print(temp)
-    print(temp2)
-    print(temp3)
-    print(temp4)
-
-
-
-#    return dates
-
-dates = ['04.11.2021', '05.11.2021-09.11.2021']
-some_date = '06.11.2021-08.11.2021'
-
-print(is_available_date(dates, some_date))
+    for stop_el in stop_num:                                                                  # главный цикл блока проверки
+        if len(stop_el) == 1:
+            if len(in_num) == 1:
+                if int(in_num[0]) == int(stop_el[0]):                                         # если списки бронирования и желаемых дат состоят из одной записи
+                    return False
+            else:    
+                if int(in_num[0]) <= int(stop_el[0]) <= int(in_num[1]):                       # если дата бронирования одна, а список желаемых - диапазон
+                    return False
+        else:
+            if len(in_num) == 1:
+                if int(stop_el[0]) <= int(in_num[0]) <= int(stop_el[1]):                      # если желаемая дата одна, а даты бронирования - диапазон
+                    return False
+            else:                                                                             # оба списка - состоят из диапазонов дат
+                if set(list(i for i in range(int(in_num[0]), int(in_num[1]) + 86400, 86400))).intersection(set(list(i for i in range(int(stop_el[0]), int(stop_el[1]) + 86400, 86400)))):
+                                                                                              # проверка условия пересечения двух множеств, составленных на основе списков бронирования и желаемых дат
+                    return False
+    return True
+    
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']        # входные условия
+some_date = '22.11.2021-25.11.2021'
+print(is_available_date(dates, some_date))                                                    # вызов основной функции
