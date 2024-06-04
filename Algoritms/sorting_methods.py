b450473250d986ccbdc2abb925b2d55db3b98b96
balu83vs <balu83vs@gmail.com>
2024-06-05 00:13:12 +0300

    sorting - done

--- Algoritms/sorting_methods.py
+++ Algoritms/sorting_methods.py
@@ -1,5 +1,6 @@
 ################################### Алгоритмы сортировок #########################################
 
+
 # Сортировка вставками (Insertion Sort)
 def insert_sorting(unsorting):
     """
@@ -20,54 +21,100 @@ def insert_sorting(unsorting):
 
 
 
-# Алгоритм сортировки слиянием
+# Сортировка выбором (Selection Sort)
+def select_sorting(unsorted):
+    """
+    В алгоритме на каждом шаге выбирается наименьший элемент из оставшихся и меняется местами 
+    с элементом, стоящим на текущей позиции. Процесс повторяется до тех пор, пока весь список 
+    не будет отсортирован.
 
-def merge(A, aux, low, mid, high):
-    # Объединить два отсортированных подсписка `A[low … mid]` и `A[mid+1 … high]`
-    k = low
-    i = low
-    j = mid + 1
-    while i <= mid and j <= high:                               # при наличии элементов в левом и правом прогонах
-        if A[i] <= A[j]:
-            aux[k] = A[i]
-            k = k + 1
+    """
+    for i in range(0, len(unsorted) - 1):
+        smallest = i
+        for j in range(i+1, len(unsorted)):
+            if unsorted[j] < unsorted[smallest]:
+                smallest = j
+        unsorted[i], unsorted[smallest] = unsorted[smallest], unsorted[i]      
+
+    return unsorted      
+
+
+
+# Быстрая сортировка (Quick Sort)
+def quick_sorting(unsorted, start, end):
+    """
+    Алгоритм использует стратегию «разделяй и властвуй». Он выбирает опорный элемент, разделяет 
+    список на две подгруппы — элементы, меньшие опорного, и элементы, большие опорного, затем 
+    рекурсивно сортирует каждую из подгрупп.
+    """
+    if end - start > 1:
+        p = partition(unsorted, start, end)
+        quick_sorting(unsorted, start, p)
+        quick_sorting(unsorted, p+1, end)
+
+
+def partition(unsorted, start, end):
+    pivot = unsorted[start]
+    i = start + 1
+    j = end - 1
+
+    while True:
+        while i <= j and unsorted[i] <= pivot:
+            i = i + 1
+        while i <= j and unsorted[j] >= pivot:
+            j = j - 1
+
+        if i <= j:
+            unsorted[i], unsorted[j] = unsorted[j], unsorted[i]
+        else:
+            unsorted[start], unsorted[j] = unsorted[j], unsorted[start]
+    
+            return j            
+
+
+
+# Сортировка слиянием (Merge Sort)
+def merge_sorting(unsorted, start, end):
+    """
+    алгоритм использует стратегию «разделяй и властвуй». Он разделяет список на две равные части, 
+    рекурсивно сортирует каждую из них, а затем объединяет две отсортированные подгруппы в одну 
+    упорядоченную последовательность.
+    """
+    if end - start > 1: 
+        mid = (start + end) // 2                     # найти среднюю точку
+ 
+        merge_sorting(unsorted, start, mid)         # разделить/объединить левую половину
+        merge_sorting(unsorted, mid, end)           # разделить/объединить правую половину
+    
+        merge(unsorted, start, mid, end)            # объединяет две половины пробега
+
+
+def merge(unsorted, start, mid, end):
+    left = unsorted[start:mid]
+    right = unsorted[mid:end]
+    k = start
+    i = 0
+    j = 0
+    
+    while start + i < mid and mid + j < end:
+        if left[i] <= right[j]:
+            unsorted[k] = left[i]
             i = i + 1
         else:
-            aux[k] = A[j]
-            k = k + 1
+            unsorted[k] = right[j] 
             j = j + 1
-    while i <= mid:                                             # скопировать оставшиеся элементы
-        aux[k] = A[i]
         k = k + 1
-        i = i + 1
- 
- # Копировать вторую половину не нужно (поскольку остальные элементы уже находятся в правильном положении во вспомогательном массиве)
- 
-    # скопировать обратно в исходный список, чтобы отразить порядок сортировки
-    for i in range(low, high + 1):
-        A[i] = aux[i]
- 
- 
-# Список сортировки `A[low…high]` с использованием вспомогательного вспомогательного списка
-def mergesort(A, aux, low, high):
- 
-    # Базовый вариант
-    if high <= low:                     #, если размер прогона <= 1
-        return
- 
-    # найти среднюю точку
-    mid = (low + ((high - low) >> 1))
- 
-    # рекурсивно разделяет прогоны на две половины до тех пор, пока размер прогона не станет <= 1,
-    #, затем объедините их и вернитесь вверх по цепочке вызовов.
- 
-    mergesort(A, aux, low, mid)         # разделить/объединить левую половину
-    mergesort(A, aux, mid + 1, high)    # разделить/объединить правую половину
- 
-    merge(A, aux, low, mid, high)       # объединяет две половины пробега
- 
- 
-   
+
+    if start + i < mid:
+        while k < end:
+            unsorted[k] = left[i]
+            i = i + 1
+            k = k + 1
+    else:
+        while k < end:
+            unsorted[k] = right[j   ]
+            j = j + 1
+            k = k + 1               
 
 
 
@@ -88,14 +135,13 @@ def isSorted(A):
 
 # Реализация алгоритмов сортировок
 if __name__ == '__main__':
-    A = [12, 3, 18, 24, 0, 5, -2]   # не отсортированная последовательность
+    A = [12, 3, 18, 24, 0, 5, -2]       # не отсортированная последовательность
 
-    insert_sorting(A)               # сортировка вставками
+    #insert_sorting(A)                  # сортировка вставками
+    #select_sorting(A)                  # сортировка выбором
+    #quick_sorting(A, 0, len(A))        # быстрая сортировка
+    merge_sorting(A, 0, len(A))         # сортировка слиянием
 
-    #aux = A.copy()
-    # отсортировать список `A`, используя вспомогательный список `aux`
-    #mergesort(A, aux, 0, len(A) - 1)
 
-
-    if isSorted(A):                 # если последовательность отсоритрована
-        print(A)                    # выводим
+    if isSorted(A):                     # если последовательность отсоритрована
+        print(A)                        # выводим
